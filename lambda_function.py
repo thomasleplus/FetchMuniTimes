a18c087f2b8c226431939543a5583c5a3b75ea3d
Thomas Leplus <thomasleplus@users.noreply.github.com>
2018-10-02 23:50:52 -0700

    Initial commit

---
+++ lambda_function.py
@@ -0,0 +1,17 @@
+import json
+import requests
+
+URL = 'https://proxy-prod.511.org/api-proxy/api/v1/transit/tracker/'
+
+def lambda_handler(event, context):
+    min = int(event['queryStringParameters']['min']) if 'min' in event['queryStringParameters'] else 0
+    max = int(event['queryStringParameters']['max']) if 'max' in event['queryStringParameters'] else 99
+    response = requests.get(url = URL, params = { 'uuid': event['queryStringParameters']['uuid'] })
+    result = []
+    for value in response.json()['real_time_departure_objects']:
+        for time in value['next_departure_time_min']:
+            itime = int(time)
+            if itime >= min and itime <= max:
+                result.append([itime, value['route_code']])
+    result.sort(key = lambda tup: tup[0] + 1 / (256 - ord(tup[1][0])))
+    return { 'statusCode': 200, 'body': json.dumps(result) }
