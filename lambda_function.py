1ec671a41bdc9297ccdd1f460601739faff5404c
Thomas Leplus <thomasleplus@users.noreply.github.com>
2021-04-02 20:44:17 -0700

    Fixing Flake8 errors

--- lambda_function.py
+++ lambda_function.py
@@ -13,12 +13,14 @@ def lambda_handler(event, context):
         max = int(event['queryStringParameters']['max'])
     else:
         max = 99
-    response = requests.get(url = URL, params = {'uuid': event['queryStringParameters']['uuid']})
+    response = \
+        requests.get(url=URL,
+                     params={'uuid': event['queryStringParameters']['uuid']})
     result = []
     for value in response.json()['real_time_departure_objects']:
         for time in value['next_departure_time_min']:
             itime = int(time)
             if itime >= min and itime <= max:
                 result.append([itime, value['route_code']])
-    result.sort(key = lambda tup: tup[0] + 1 / (256 - ord(tup[1][0])))
+    result.sort(key=lambda tup: tup[0] + 1 / (256 - ord(tup[1][0])))
     return {'statusCode': 200, 'body': json.dumps(result)}
