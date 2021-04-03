ab780f25763c6b422756507e336756c0d0ab5676
Thomas Leplus <thomasleplus@users.noreply.github.com>
2021-04-02 20:41:49 -0700

    Fixing Flake8 errors

--- lambda_function.py
+++ lambda_function.py
@@ -3,10 +3,17 @@ import requests
 
 URL = 'https://proxy-prod.511.org/api-proxy/api/v1/transit/tracker/'
 
+
 def lambda_handler(event, context):
-    min = int(event['queryStringParameters']['min']) if 'min' in event['queryStringParameters'] else 0
-    max = int(event['queryStringParameters']['max']) if 'max' in event['queryStringParameters'] else 99
-    response = requests.get(url = URL, params = { 'uuid': event['queryStringParameters']['uuid'] })
+    if 'min' in event['queryStringParameters']:
+        min = int(event['queryStringParameters']['min'])
+    else:
+        min = 0
+    if 'max' in event['queryStringParameters']:
+        max = int(event['queryStringParameters']['max'])
+    else:
+        max = 99
+    response = requests.get(url = URL, params = {'uuid': event['queryStringParameters']['uuid']})
     result = []
     for value in response.json()['real_time_departure_objects']:
         for time in value['next_departure_time_min']:
@@ -14,4 +21,4 @@ def lambda_handler(event, context):
             if itime >= min and itime <= max:
                 result.append([itime, value['route_code']])
     result.sort(key = lambda tup: tup[0] + 1 / (256 - ord(tup[1][0])))
-    return { 'statusCode': 200, 'body': json.dumps(result) }
+    return {'statusCode': 200, 'body': json.dumps(result)}
